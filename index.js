e385df60406158725e3b7eeded34ffcfa95555f8
Michel-bgc <michel@bgcbrasil.com.br>
2023-06-05 19:26:18 -0300

    Rising a hello world with vanillaJS

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+let server;
+function startServer() {
+    server = http.createServer((req, res) => {
+        if (req.url === "/") {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end("<h1>Hello world</h1>");
+        }
+    });
+
+    server.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+
+    server.on('close', () => {
+        console.log('Server stopped');
+        startServer();
+    });
+}
+
+
+
+startServer();

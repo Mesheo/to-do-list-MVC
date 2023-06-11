0a71059de528bac73ddc70d2ef198584e900a319
Michel-bgc <michel@bgcbrasil.com.br>
2023-06-11 09:54:27 -0300

    Checkpoint_2: mini refactor to use promises instead of callbacks

--- routes/route.js
+++ routes/route.js
@@ -1,42 +1,41 @@
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
 const ejs = require("ejs");
 const querystring = require("querystring");
 
-function servirBrowser(
+async function readFile(caminhoProArquivo) {
+    try {
+        const data = await fs.readFile(caminhoProArquivo);
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function servirBrowser(
     objetoDeRequisicao,
     objetoDeResposta,
     caminhoProArquivo,
     contentType
 ) {
     if (objetoDeRequisicao.method == "POST") {
-        console.log("CADE OS DADOS!!!!!!!!!!");
         objetoDeRequisicao.on("data", (data) => {
             inputData = data.toString();
-            console.log("Ta aqui: ", querystring.parse(inputData));
+            console.log("-- DADOS da chamada: ", querystring.parse(inputData));
         });
         objetoDeResposta.end(`
         <script>alert('Obrigado por enviar o formulario!');</script>
     `);
     } else if (objetoDeRequisicao.method == "GET") {
-        filePath = path.join(__dirname, "..", caminhoProArquivo);
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log("Deu ruim lendo arquivo: ", err);
-                objetoDeResposta.end();
-            } else if (objetoDeRequisicao.method == "GET") {
-                console.log(
-                    "Metodo do Objeto de requisicao: ",
-                    objetoDeRequisicao.method
-                );
-                objetoDeResposta.setHeader("Content-type", contentType);
-                console.log(
-                    `---- Headers da resposta ${caminhoProArquivo}: `,
-                    JSON.stringify(objetoDeResposta.getHeaders())
-                );
-                objetoDeResposta.end(data);
-            }
-        });
+        try {
+            data = await readFile(caminhoProArquivo);
+            objetoDeResposta.setHeader("Content-type", contentType);
+            objetoDeResposta.end(data);
+        } catch (e) {
+            objetoDeResposta.setHeader("statusCode", 500);
+            objetoDeResposta.end("ERRO NO SERVER!");
+            console.log("Deu ruim lendo arquivo: ", err);
+        }
     }
 }
 
